c58fa131bf2ff071d54f0f9599d5f52e3d52f04c
Rizabetta <liza.popechiteleva@mail.ru>
2021-07-01 21:29:40 +0400

    highlighting the icon of the selected mode

--- Task2/src/index.js
+++ Task2/src/index.js
@@ -1,1 +1,32 @@
-console.log('hello again')
+const allvoices = document.querySelector('.allvoices');
+const microphone = document.querySelector('.microphone');
+const stream = document.querySelector('.stream');
+
+const iconvalues = document.querySelector('.iconvalues');
+
+const inputactive = document.querySelector('.activereplacement');
+
+allvoices.onclick = function () {
+    inputactive.textContent = 'allvoices - mode';
+
+    allvoices.classList.add("selecteditem");
+    microphone.classList.remove("selecteditem");
+    stream.classList.remove("selecteditem");
+
+};
+
+microphone.onclick = function () {
+    inputactive.textContent = 'speaker - mode';
+
+    microphone.classList.add("selecteditem");
+    allvoices.classList.remove("selecteditem");
+    stream.classList.remove("selecteditem");
+};
+
+stream.onclick = function () {
+    inputactive.textContent = 'stream - mode';
+
+    stream.classList.add("selecteditem");
+    allvoices.classList.remove("selecteditem");
+    microphone.classList.remove("selecteditem");
+};
