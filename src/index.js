ba3fe02e1de8177242cca8934bb376d5507fa294
Rizabetta <liza.popechiteleva@mail.ru>
2021-07-09 21:49:00 +0400

    recording from microphone in div messagesallvoices

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ const stream = document.querySelector('.stream');
 
 const inputactive = document.querySelector('.activereplacement');
 
+const button = document.querySelector('.button');
+
+var button2, context, analyser, src;
+
+document.getElementById('messagesallvoices').hidden = true;
+document.getElementById('messagesmicrophone').hidden = true;
+document.getElementById('messagesstream').hidden = true;
+
 function changeclass(activemode) {
     allvoices.classList.remove("selecteditem");
     microphone.classList.remove("selecteditem");
     stream.classList.remove("selecteditem");
     activemode.classList.add("selecteditem");
+}
+
+function hideelements(activemode) {
+    document.getElementById('messagesallvoices').hidden = true;
+    document.getElementById('messagesmicrophone').hidden = true;
+    document.getElementById('messagesstream').hidden = true;
+    document.getElementById(activemode).hidden = false;
 }
 
 allvoices.onclick = function () {
@@ -18,8 +33,35 @@ allvoices.onclick = function () {
 
 microphone.onclick = function () {
     inputactive.textContent = 'speaker - mode';
+    hideelements('messagesmicrophone');
     changeclass(this);
-};
+
+    const start = document.getElementById('start');
+    const stop = document.getElementById('stop');
+    const div = document.getElementById('messagesallvoices');
+
+    start.onclick = () => {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(stream => {
+                const mediaRecorder = new MediaRecorder(stream);
+                mediaRecorder.start();
+                let chuck = [];
+                mediaRecorder.addEventListener('dataavailable', e => {
+                    chuck.push(e.data);
+                });
+                mediaRecorder.addEventListener('stop', e => {
+                    const blob = new Blob(chuck);
+                    const audio_url = URL.createObjectURL(blob);
+                    const audio = new Audio(audio_url);
+                    audio.setAttribute('controls', 1);
+                    div.appendChild(audio);
+                });
+                stop.onclick = () => {
+                    mediaRecorder.stop();
+                };
+            });
+    };
+}
 
 stream.onclick = function () {
     inputactive.textContent = 'stream - mode';
